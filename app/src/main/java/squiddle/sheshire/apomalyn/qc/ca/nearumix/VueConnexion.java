c42259ac415a89ecf47112ab9af942e557133000
apomalyn <chretienxavier42@gmail.com>
2017-09-28 16:22:46 -0400

    Initial commit

---
+++ app/src/main/java/squiddle/sheshire/apomalyn/qc/ca/nearumix/VueConnexion.java
@@ -0,0 +1,13 @@
+package squiddle.sheshire.apomalyn.qc.ca.nearumix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VueConnexion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_connexion);
+    }
+}
