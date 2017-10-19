01b6d6ee24c726cb8687a24c04a77fef49340b29
sheshire <laurie.fernanli@gmail.com>
2017-10-19 18:16:53 -0400

    correctif qrcode

--- app/src/main/java/squiddle/sheshire/apomalyn/qc/ca/nearumix/VueQRCode.java
+++ app/src/main/java/squiddle/sheshire/apomalyn/qc/ca/nearumix/VueQRCode.java
@@ -30,6 +30,7 @@ public class VueQRCode extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_qrcode);
+        utilisateurDAO = UtilisateurDAO.getInstance();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
